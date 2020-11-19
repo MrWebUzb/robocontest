d142f9f0058e05e916a71216ce9ef28eb6b4e774
MrWebUzb <space.coding.programmer@gmail.com>
2020-11-20 04:30:53 +0500

    Fibonacci oxirgi raqam masalasi yechildi

---
+++ fibonacci_oxirgi_raqam/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func printf(f string, a ...interface{}) { fmt.Fprintf(writer, f, a...) }
+func scanf(f string, a ...interface{})  { fmt.Fscanf(reader, f, a...) }
+
+func main() {
+	defer writer.Flush()
+
+	cache := []int{0, 1, 1, 2, 3, 5, 8, 3, 1, 4, 5, 9, 4, 3, 7, 0, 7, 7, 4, 1, 5, 6, 1, 7, 8, 5, 3, 8, 1, 9, 0, 9, 9, 8, 7, 5, 2, 7, 9, 6, 5, 1, 6, 7, 3, 0, 3, 3, 6, 9, 5, 4, 9, 3, 2, 5, 7, 2, 9, 1, 0}
+
+	var t int
+	var n uint64
+	scanf("%d\n", &t)
+
+	for t > 0 {
+		scanf("%d\n", &n)
+		printf("%d\n", cache[int(n%60)])
+		t--
+	}
+}
